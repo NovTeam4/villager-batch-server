14705546b434c5ff87471a6ff6f9a4e497f5763a
donghun93 <alswn4516@gmail.com>
2022-12-23 17:53:28 +0900

    health check api 추가

---
+++ src/main/java/com/villager/batchserver/controller/BatchServerApiController.java
@@ -0,0 +1,16 @@
+package com.villager.batchserver.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1")
+public class BatchServerApiController {
+
+    @GetMapping("/healthcheck")
+    public ResponseEntity<String> healthCheck() {
+        return ResponseEntity.ok("ok");
+    }
+}
