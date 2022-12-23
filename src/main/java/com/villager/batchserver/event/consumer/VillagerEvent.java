1d2735e3ff67489c91b901f98f49cac0521ac54c
donghun93 <alswn4516@gmail.com>
2022-12-23 17:53:18 +0900

    코드 정리

--- src/main/java/com/villager/batchserver/event/consumer/VillagerEvent.java
+++ src/main/java/com/villager/batchserver/event/consumer/VillagerEvent.java
@@ -1,14 +1,12 @@
 package com.villager.batchserver.event.consumer;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
+@ToString
 public class VillagerEvent {
     private VillagerEventType eventType;
     private long timestamp;
